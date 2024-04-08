50e97b3911403a36f94a5eed89845f926726655e
Jesse Jurman <j.r.jurman@gmail.com>
2024-04-08 00:58:34 -0400

    new component definitions, remove pico-css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // function to get the total Effect points available
 function getTotalAvailableEP() {
 	// get EP from selected table button
-	const startingEP = epTable.querySelector('[selected]')?.getAttribute('value') || '0';
+	const startingEP = baseEPRadioGroup.getAttribute('value') || '0';
 
 	// check if isAce is checked
 	const aceEP = isAce.hasAttribute('selected') ? 1 : 0;
@@ -27,23 +27,15 @@ function updateEPLabel() {
 }
 
 // update the top baseline EP table to trigger the `updateEPLabel`
-const tableButtons = window.epTable.querySelectorAll('button');
-tableButtons.forEach((button) => {
-	button.addEventListener('click', (event) => {
-		tableButtons.forEach((button) => button.removeAttribute('selected'));
-		event.target.setAttribute('selected', '');
-	});
-});
+const tableButtons = window.epTable.querySelectorAll('ep-radio-button');
 tableButtons.forEach((button) => button.addEventListener('click', updateEPLabel));
 
 // update the Ace button to also trigger the `updateEPLabel`
-isAce.addEventListener('click', () => {
-	isAce.toggleAttribute('selected');
-});
 isAce.addEventListener('click', updateEPLabel);
 
-// on any of the effect point selectors updating, update the EP cost
-const epEffectInputs = document.querySelectorAll('[ep-input]');
-epEffectInputs.forEach((input) => {
-	input.addEventListener('change', updateEPLabel);
+// on any of the effect selections being focused, update the side-panel
+[...effectSelection.children].forEach((effectControl) => {
+	effectControl.addEventListener('focus', (event) => {
+		console.log(event.target);
+	});
 });
